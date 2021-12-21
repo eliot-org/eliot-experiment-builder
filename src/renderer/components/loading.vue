32c4734f88c6496ee0ee7c4d4ea964e1061e05ea
hilgerto <tom.hilgers@imat-uve.de>
2021-12-21 15:50:17 +0100

    Loading Page changed

--- src/renderer/components/loading.vue
+++ src/renderer/components/loading.vue
@@ -1,11 +1,31 @@
 <template>
-    <div class="loading-wrapper">
-        <img class="logo" src="~@/assets/loading.gif" alt="Loading">   
+    <div class="loading-wrapper">  
+        <div class="centerarea">
+            <img class="logo" src="~@/assets/eliot.png" alt="ELIOT"> 
+            <h1 style="font-size:3rem; margin-bottom:10px">ELIOT</h1>
+            <p style="font-size: 0.9rem">
+                Version 0.1<br>
+                Blueberry<br>
+            </p>
+        </div>
+
+        <div class="footer">
+            Developed by <a class="link" href="mailto:tom.hilgers@unitybox.de">Tom Hilgers</a>, <a class="link" href="mailto:leander.schweiger@posteo.de">Leander Schweiger</a><br>
+            License Holder:  <a @click="openLink" class="link" href="https://imat-uve.de">imat-uve gmbh</a> 
+        </div>
     </div>
 </template>
 
 <script>
+const shell = require('electron').shell;
 export default {
+    methods: {
+        openLink: function(event){
+            event.preventDefault()
+            console.log(event.srcElement.href)
+            shell.openExternal(event.srcElement.href)
+        }
+    },
     mounted(){ 
         /**
          * Called when the Main process sends on the surveyOps channel. arg includes the data to which page this instance should go to. So if it is a admin or surveypage
@@ -18,7 +38,9 @@ export default {
                 }
             }else if(arg == "goToLogin"){
                 if (this.$router.currentRoute.name != "SurveyWelcome") {
-                    this.$router.push({ name: "AdminPage"})
+                    setTimeout(() => {
+                        this.$router.push({ name: "AdminPage"})
+                    }, 1000)
                 }
             }
         })
@@ -35,7 +57,29 @@ export default {
         display: flex;
     }
 
+    .footer{
+        position: absolute;
+        bottom: 0px;
+        width: 100%;
+        text-align: center;
+        font-size: 0.7rem;
+        padding-bottom: 15px;
+    }
+
+    .link{
+        color:black
+    }
+
     .logo{
+        width: 350px
+    }
+
+    .centerarea{
+        width: 100%;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        text-align: center;
         margin: auto;
     }
 </style>
